023850c79d62921eca3330976079f8021195cce2
Lumidee89 <97644187+Lumidee89@users.noreply.github.com>
2024-09-04 09:58:20 +0100

    ok

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,40 +1,40 @@
-// const jwt = require('jsonwebtoken');
-
-// const authMiddleware = (req, res, next) => {
-//     const token = req.headers.authorization?.split(' ')[1];
-
-//     if (token) {
-//         try {
-//             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//             req.userId = decoded.id; // Attach userId to request
-//             next();
-//         } catch (error) {
-//             return res.status(401).json({ message: 'Invalid or expired token' });
-//         }
-//     } else {
-//         return res.status(401).json({ message: 'Authorization token is required' });
-//     }
-// };
-
-// module.exports = authMiddleware;
-
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
 
-const authMiddleware = async (req, res, next) => {
-    const token = req.headers['authorization']?.split(' ')[1];
+const authMiddleware = (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
 
-    if (!token) {
-        return res.status(401).json({ msg: 'No token provided' });
-    }
-
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = await User.findById(decoded.userId).select('-password'); // Exclude password from the response
-        next();
-    } catch (error) {
-        res.status(401).json({ msg: 'Invalid token' });
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.userId = decoded.id; // Attach userId to request
+            next();
+        } catch (error) {
+            return res.status(401).json({ message: 'Invalid or expired token' });
+        }
+    } else {
+        return res.status(401).json({ message: 'Authorization token is required' });
     }
 };
 
 module.exports = authMiddleware;
+
+// const jwt = require('jsonwebtoken');
+// const User = require('../models/User');
+
+// const authMiddleware = async (req, res, next) => {
+//     const token = req.headers['authorization']?.split(' ')[1];
+
+//     if (!token) {
+//         return res.status(401).json({ msg: 'No token provided' });
+//     }
+
+//     try {
+//         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//         req.user = await User.findById(decoded.userId).select('-password'); // Exclude password from the response
+//         next();
+//     } catch (error) {
+//         res.status(401).json({ msg: 'Invalid token' });
+//     }
+// };
+
+// module.exports = authMiddleware;
