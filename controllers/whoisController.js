b1d156c6f6960a159206f539bb0fc8af8e965839
Lumidee89 <97644187+Lumidee89@users.noreply.github.com>
2024-11-19 13:53:56 +0100

    ok

--- controllers/whoisController.js
+++ controllers/whoisController.js
@@ -1,35 +1,42 @@
 const whois = require('whois');
 const DomainDetails = require('../models/domainDetails');
 
+const whoisServers = {
+  '.in': 'whois.registry.in',
+  '.uk': 'whois.nic.uk',
+  '.au': 'whois.auda.org.au',
+  '.ca': 'whois.cira.ca',
+  '.jp': 'whois.jprs.jp',
+  '.cn': 'whois.cnnic.cn',
+};
+
 exports.lookup = (req, res) => {
   const { domain } = req.params;
-
   if (!domain) {
     return res.status(400).json({ msg: 'Domain is required' });
   }
-
   if (!req.user || !req.user._id) {
     return res.status(401).json({ msg: 'Unauthorized. Please login to save domain details' });
   }
-
-  whois.lookup(domain, (err, data) => {
+  const tld = domain.slice(domain.lastIndexOf('.'));
+  const whoisServer = whoisServers[tld];
+  const options = whoisServer ? { server: whoisServer } : undefined;
+  whois.lookup(domain, options, (err, data) => {
     if (err) {
       return res.status(500).json({ msg: 'Error performing WHOIS lookup', error: err });
     }
-
     const formattedData = formatWhoisData(data);
-
     const domainDetails = new DomainDetails({
       domain,
       whoisData: formattedData,
-      user: req.user._id
+      user: req.user._id,
     });
-
-    domainDetails.save()
-      .then(savedDomain => {
+    domainDetails
+      .save()
+      .then((savedDomain) => {
         res.status(200).json({ domain, whoisData: formattedData, savedDomain });
       })
-      .catch(error => {
+      .catch((error) => {
         console.error('Error saving domain details:', error);
         res.status(500).json({ msg: 'Error saving domain details to the database' });
       });
@@ -39,12 +46,10 @@ exports.lookup = (req, res) => {
 const formatWhoisData = (data) => {
   const lines = data.split('\n');
   const formattedData = {};
-
-  lines.forEach(line => {
+  lines.forEach((line) => {
     const [key, ...value] = line.split(':');
     const formattedKey = key ? key.trim() : '';
     const formattedValue = value.join(':').trim();
-
     if (formattedKey) {
       switch (formattedKey) {
         case 'Domain Name':
@@ -72,17 +77,15 @@ const formatWhoisData = (data) => {
       }
     }
   });
-
   return cleanUpData(formattedData);
 };
 
 const cleanUpData = (data) => {
   const cleanedData = {};
-
-  Object.keys(data).forEach(key => {
+  Object.keys(data).forEach((key) => {
     if (data[key] && typeof data[key] === 'object') {
       cleanedData[key] = {};
-      Object.keys(data[key]).forEach(subKey => {
+      Object.keys(data[key]).forEach((subKey) => {
         if (data[key][subKey]) {
           cleanedData[key][subKey] = data[key][subKey];
         }
@@ -91,6 +94,5 @@ const cleanUpData = (data) => {
       cleanedData[key] = data[key];
     }
   });
-
   return cleanedData;
 };
